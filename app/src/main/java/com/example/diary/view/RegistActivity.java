6287af3633fda7c2257183c2115b66e94c83502f
sadcoincidence <1608933213@qq.com>
2023-05-24 02:29:39 +0800

    0

--- app/src/main/java/com/example/diary/view/RegistActivity.java
+++ app/src/main/java/com/example/diary/view/RegistActivity.java
@@ -35,7 +35,7 @@ public class RegistActivity extends Activity {
                 }
                 else
                 {
-                    showDialog();
+                    showDialog();//tanchuang
                 }
                 Util.regist(RegistActivity.this, name.getText().toString(),pwd.getText().toString());
 
