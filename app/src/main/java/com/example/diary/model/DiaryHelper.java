6c292693645cb82837628e77d0777612543ab589
ashgd <130276151+ashgd@users.noreply.github.com>
2023-05-25 11:49:17 +0800

    Update DiaryHelper.java

--- app/src/main/java/com/example/diary/model/DiaryHelper.java
+++ app/src/main/java/com/example/diary/model/DiaryHelper.java
@@ -6,7 +6,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
+import android.util.Log; 
 
 import androidx.annotation.Nullable;
 
